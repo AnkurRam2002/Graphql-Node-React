e535ff4470b6e83c0cb37fe9f5750a7ea37a75bc
AnkurRam2002 <ankurram2002@gmail.com>
2024-07-03 21:21:37 +0530

    finalized graphql data fetching and added bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client';
+import Table from 'react-bootstrap/Table';
 
-const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',
-  cache: new InMemoryCache()
-});
-
-const  query = `
-query ExampleQuery {
-  getTodos{
-    title
-    user{
-      name
+const  query = gql`
+  query getTodos {
+    getTodos {
+      id
+      title
+      completed
+      user {
+        name
+        email
+      }
     }
   }
-}`
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(query);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
+
+    console.log(data.getTodos);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table striped bordered hover>
+        <tbody>
+          <tr>
+            <th>Title</th>
+            <th>Completed</th>
+            <th>User</th>
+          </tr>
+          {data.getTodos.map(todo => (
+            <tr key={todo.id}>
+              <td>{todo.title}</td>
+              <td>{todo.completed ? 'Completed' : 'Not Completed'}</td>
+              <td>{todo.user.name} ({todo.user.email})</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 }
